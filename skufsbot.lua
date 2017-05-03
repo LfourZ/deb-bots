bd5959e9818be6ceaf2dd99b0bce8f3c76b75a71
Lazor <l4@Argon.local>
2017-05-03 11:28:19 +0200

    comit mesage

--- skufsbot.lua
+++ skufsbot.lua
@@ -1,9 +1,11 @@
 local discordia = require("discordia")
 local client = discordia.Client()
 local token = require("../token")
-local xml = require("libs/xml").newParser()
 local http = require("coro-http")
+dofile("libs/xml.lua")
+dofile("libs/handler.lua")
 
+local xml = xmlParser(simpleTreeHandler())
 
 client:on("ready", function()
 	p(string.format("Logged in as %s", client.user.username))
@@ -15,7 +17,7 @@ client:on("messageCreate", function(message)
 	cmd = cmd or message.content
 	if cmd == "test" then
 		local res, data = http.request("GET", "http://stats.skufs.net/api/serverinfo/178.63.73.69:27017")
-		data = xml:ParseXmlText(data)
+		data = xml:parse(data)
 		local str = ""
 		for k, v in pairs(data) do
 			str = str..tostring(k).."  "..tostring(v).."\n"
